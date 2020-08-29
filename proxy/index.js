09072d3339c1a3d1f3923e3eb5518c03ac10ca6a
ChrisLin <i100308326@gmail.com>
2020-08-29 23:23:59 +0800

    Proxy、Reflect

--- proxy/index.js
+++ proxy/index.js
@@ -17,6 +17,63 @@ student = new Proxy(student, {
 
 student.name = '' // this name is invalid
 student.name = 'chris lin' // pass
-
 student.phone = '1234567890' // this phone is invalid
 student.phone = '0955607278' // pass
+
+
+// can not add new property
+let user = {
+  name: 'chris',
+  age: 23,
+  phone: '0912345678',
+}
+
+user = new Proxy(user, {
+  set (target, key, value) {
+    if (target.hasOwnProperty(key)) Reflect.set(target, key, value)
+    else console.log('you can not add other property')
+  },
+})
+
+user.aa = 00 // you can not add other property
+user.name = 123 // pass
+
+
+
+
+// you can keep private property
+let bankAccount = {
+  _api_token: '6xt1931toy13931293',
+  _money: 5000000,
+
+  // use private method change ...
+  deposit (value) {
+    this._money += value
+  }
+}
+
+bankAccount = new Proxy(bankAccount, {
+  set (target, key) {
+    if (key.includes('_')) {
+      console.log('can not set this property')
+      return Reflect.set(target)
+    }
+  }
+})
+
+bankAccount._money = 10000000 // can not set this property
+console.log(bankAccount) // { _api_token: '6xt1931toy13931293', _money: 5000000, deposit: [Function: deposit], }
+
+
+
+
+// Proxy revocable
+let revocable = Proxy.revocable({}, {
+  set (target, key) {
+    return target[key] = 'nothing'
+  }
+})
+
+revocable // {proxy: Proxy, revoke: f }
+
+revocable.revoke() // cancel proxy
