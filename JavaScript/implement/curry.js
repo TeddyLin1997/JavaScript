9e50f0b6b99fa36499477e4e5dbe18a505e70c0e
ChrisLin <i100308326@gmail.com>
2023-01-24 12:34:47 +0800

    Fix: curry vs partial

--- JavaScript/implement/curry.js
+++ JavaScript/implement/curry.js
@@ -1,15 +1,25 @@
-function curry (fn) {
-  return function (...args) {
-    if (fn.length !== args.length) return fn.bind(null, ...args)
-    return fn(...args)
+function curry (func) {
+  const params = []
+  const length = func.length
+
+  const next = (param) => {
+    params.push(param)
+
+    if (params.length === length) return func(...params)
+    else return next
   }
+
+  return next
 }
 
 // test
 const sum = (a, b) => a + b
-const fn = curry(sum)
+const fn1 = curry(sum)
+const fn2 = curry(sum)
+const fn3 = curry(sum)
 
-console.assert(typeof fn === 'function', 'test 1')
-console.assert(typeof fn(1) === 'function', 'test 2')
-console.assert(typeof fn(1)(2) === 'number', 'test 3')
-console.assert(fn(1)(2) === 3, 'test 4')
+console.assert(typeof fn1 === 'function', 'test 1')
+console.assert(typeof fn1(1) === 'function', 'test 2')
+console.assert(fn1(2) === 3, 'test 3')
+console.assert(fn2(1)(2) === 3, 'test 4')
+console.assert(typeof fn3()() === 'number', 'test 5')
